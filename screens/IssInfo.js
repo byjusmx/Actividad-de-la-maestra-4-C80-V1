c01c3ea8a5a336be0e68cb0b8ef61a05c675c266
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-12-02 20:02:36 -0600

    Update IssInfo.js

--- screens/IssInfo.js
+++ screens/IssInfo.js
@@ -52,10 +52,10 @@ export default class IssLocationScreen extends Component {
         } else {
             return (
                 <View style={styles.infoContainer}>
-                    <Text style={styles.infoText}>Latitude: {this.state.location.latitude}</Text>
-                    <Text style={styles.infoText}>Longitude: {this.state.location.longitude}</Text>
-                    <Text style={styles.infoText}>Altitude (KM): {this.state.location.altitude}</Text>
-                    <Text style={styles.infoText}>Velocity (KM/H): {this.state.location.velocity}</Text>
+                    <Text style={styles.infoText}>Latitud: {this.state.location.latitude}</Text>
+                    <Text style={styles.infoText}>Longitud: {this.state.location.longitude}</Text>
+                    <Text style={styles.infoText}>Altitud (KM): {this.state.location.altitude}</Text>
+                    <Text style={styles.infoText}>Velocidad (KM/H): {this.state.location.velocity}</Text>
                 </View>
             );
         }
