96649c748a8638f2d8c76d5307f08fc11f1db8d6
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-12-02 20:15:19 -0600

    Update Meteors.js

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -62,7 +62,7 @@ export default class MeteorScreen extends Component {
                             <Text style={[styles.cardTitle, { marginTop: 400, marginLeft: 50 }]}>{item.name}</Text>
                             <Text style={[styles.cardText, { marginTop: 20, marginLeft: 50 }]}>Más cercano a la Tierra - {item.close_approach_data[0].close_approach_date_full}</Text>
                             <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diámetro mínimo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_min}</Text>
-                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diámetro maximo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_max}</Text>
+                            <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diámetro máximo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_max}</Text>
                             <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Velocidad (KM/H) - {item.close_approach_data[0].relative_velocity.kilometers_per_hour}</Text>
                             <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Casi choca con la Tierra por (KM) - {item.close_approach_data[0].miss_distance.kilometers}</Text>
                         </View>
